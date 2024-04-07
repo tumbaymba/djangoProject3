6017af45b6832c3feb60abcb2c176c67b08bb020
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-04-07 14:43:19 +0300

    Correct 20.1 (DZ_3)

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -1,23 +1,34 @@
 from django.core.management import BaseCommand
-
-from catalog.models import Category
+import json
+from catalog.models import Category, Product
+from django.db import connection
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        category_list = [
-            {'category_name': 'овощи', 'description': 'огурцы, помидоры и пр.'},
-            {'category_name': 'фрукты', 'description': 'апельсины, яблоки и пр.'},
-            {'category_name': 'кондитерские изделия', 'description': 'торты, выпечка и пр.'},
-            {'category_name': 'зелень', 'description': 'укроп, петрушка и пр.'},
-        ]
-
-        category_for_create = []
-        for category_item in category_list:
-            category_for_create.append(
-                Category(**category_item)
-            )
+        with connection.cursor() as cursor:
+            cursor.execute(f'TRUNCATE TABLE catalog_category RESTART IDENTITY CASCADE')
 
         Category.objects.all().delete()
-        Category.objects.bulk_create(category_for_create)
+        Product.objects.all().delete()
+
+        with open('data.json') as json_file:
+            data = json.load(json_file)
+
+            product_for_create = []
+            category_for_create = []
+
+            for category in data:
+                if category['model'] == 'catalog_category':
+                    category_for_create.append(Category(category_name=category['fields']['category_name'],
+                                                        category_description=category['fields'][
+                                                            'category_description']))
+            Category.objects.bulk_create(category_for_create)
+            for product in data:
+                if product['model'] == 'product_for_create':
+                    product_for_create.append(Product(product_name=product['fields']['product_name'],
+                                                      description=product['fields']['description'],
+                                                      category=Category.objects.get(pk=product['fields']['category']),
+                                                      price=product['fields']['price']))
+            Product.objects.bulk_create(product_for_create)
--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 
-
 # Create your models here.
-class Product(models.Model):
-    product_name = models.CharField(max_length=150, verbose_name='Имя')
-    description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='media/product_images/', verbose_name='Изображение', null=True, blank=True)
-    product_category = models.CharField(max_length=150, verbose_name='Категория')
-    product_price = models.IntegerField(verbose_name='Цена')
-    date_creation = models.DateField(verbose_name='Дата создания')
-    date_last_change = models.DateField(verbose_name='Дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.product_name}, {self.description}, {self.product_price}'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-        ordering = ('product_name',)
 
 
 class Category(models.Model):
@@ -31,3 +14,23 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('category_name',)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=150, verbose_name='Имя')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='media/product_images/', verbose_name='Изображение', null=True, blank=True)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    product_price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.product_name}, {self.description}, {self.product_price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('product_name',)
+
+
