c5f597bda88ec1bf7f283cfaa6edfb5bf0943690
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-05-26 20:42:11 +0300

    DZ_22.2 from 26.05

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+
+from users.models import User
+from django import forms
+
+
+class LoginCustomForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+
+class UserRegisterForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+    class Meta:
+        model = User
+        fields = ('email', 'password1', 'password2',)
+
+
+class PasswordRecoveryForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
+    class Meta:
+        model = User
+        fields = ('email',)
